9bab4670c3926411bf2c204302b7172d34948536
Jesse Shawl <jesse@jshawl.com>
2015-11-10 15:18:22 -0500

    add boards

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,8 @@
+class BoardsController < ApplicationController
+  def index
+    @boards = Board.all
+  end
+  def show
+    @board = Board.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Board.destroy_all
+Board.create([
+  {title:"Books Worth Reading"},
+  {title:"Gardening"}
+])
